5607bb0bee06686481a0ceea0dcd3f95d9e51bc1
zhang <969253189@qq.com>
2018-06-08 00:41:36 +0800

    保存

--- 08-网络爬虫/request/urllib_test.py
+++ 08-网络爬虫/request/urllib_test.py
@@ -1,7 +1,5 @@
 import urllib.request
 
-
-
 url = 'http://www.baidu.com'
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36'
